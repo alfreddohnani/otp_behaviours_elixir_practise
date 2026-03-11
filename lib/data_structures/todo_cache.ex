ec01693fecb2667f0cd8de1e1150d367904ca301
alfreddohnani <alfreddohnani@hotmail.com>
2026-03-11 15:21:53 +0000

    convert  todocache to a dynamic supervisor for todo server processes

--- lib/data_structures/todo_cache.ex
+++ lib/data_structures/todo_cache.ex
@@ -1,36 +1,58 @@
 defmodule DataStructures.TodoCache do
   alias DataStructures.GenServerTodoList
-  use GenServer
+  # use GenServer
 
-  @impl GenServer
-  def init(_) do
-    IO.puts("Starting todo cache.")
-    {:ok, %{}}
-  end
+  # @impl GenServer
+  # def init(_) do
+  #   IO.puts("Starting todo cache.")
+  #   {:ok, %{}}
+  # end
 
-  @impl GenServer
-  def handle_call({:server_process, todo_list_name}, _from, todo_servers) do
-    case Map.fetch(todo_servers, todo_list_name) do
-      {:ok, todo_server} ->
-        {:reply, todo_server, todo_servers}
+  # @impl GenServer
+  # def handle_call({:server_process, todo_list_name}, _from, todo_servers) do
+  #   case Map.fetch(todo_servers, todo_list_name) do
+  #     {:ok, todo_server} ->
+  #       {:reply, todo_server, todo_servers}
 
-      :error ->
-        {:ok, new_server} = GenServerTodoList.start_link(todo_list_name)
+  #     :error ->
+  #       {:ok, new_server} = GenServerTodoList.start_link(todo_list_name)
 
-        {
-          :reply,
-          new_server,
-          Map.put(todo_servers, todo_list_name, new_server)
-        }
-    end
-  end
+  #       {
+  #         :reply,
+  #         new_server,
+  #         Map.put(todo_servers, todo_list_name, new_server)
+  #       }
+  #   end
+  # end
 
-  def start_link(_) do
-    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  def start_link() do
+    IO.puts("Starting todo cache")
+
+    DynamicSupervisor.start_link(
+      name: __MODULE__,
+      strategy: :one_for_one
+    )
   end
 
   def server_process(todo_list_name) do
-    IO.puts("Starting todo server process for #{todo_list_name}")
-    GenServer.call(__MODULE__, {:server_process, todo_list_name})
+    case start_child(todo_list_name) do
+      {:ok, pid} -> pid
+      {:error, {:already_started, pid}} -> pid
+    end
+  end
+
+  defp start_child(todo_list_name) do
+    DynamicSupervisor.start_child(
+      __MODULE__,
+      {GenServerTodoList, todo_list_name}
+    )
+  end
+
+  def child_spec(_arg) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, []},
+      type: :supervisor
+    }
   end
 end
